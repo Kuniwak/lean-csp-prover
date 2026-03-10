233d442a8729c2239d68413fe9ddd4547cc60ce3
Kuniwak <yuki.kokubun@coincheck.com>
2026-03-11 06:49:34 +0900

    Port DFP_Main

--- LeanCspProver/DFP/DFP_Main.lean
+++ LeanCspProver/DFP/DFP_Main.lean
@@ -1,18 +1,11 @@
            /- -------------------------------------------*
             |                DFP package                |
             |                 June 2005                 |
-            |                                           |
-            |        CSP-Prover on Isabelle2009         |
-            |                 June 2009  (modified)     |
-            |                                           |
-            |        CSP-Prover on Isabelle2009-1       |
-            |                January 2010  (modified)   |
-            |                                           |
             |        Yoshinao Isobe (AIST JAPAN)        |
             *------------------------------------------- -/
 
-import LeanCspProver.DFP.DFP_Main
-import LeanCspProver.CSP_F.CSP_F
+import LeanCspProver.DFP.DFP_Proof_Rule1
+import LeanCspProver.DFP.DFP_DFtick
 
 /- *****************************************************************
 
